cff0b7dd25d9ab80a112ea816a1684198358a145
lelequinter <lele.quinter@gmail.com>
2023-05-11 22:07:53 -0500

    avance de animacion collapse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import * as _ from 'lodash';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnDestroy {
   title = 'test';
 
   form: FormGroup = this.fb.group({
-    test: [null, Validators.required],
+    test: ['null', Validators.required],
     check: [false],
+    name: ['leyder'],
+    apellido: ['quintero'],
   });
 
   regex =
@@ -24,30 +38,28 @@ export class AppComponent implements AfterViewInit {
 
   beneficiaries_per_disease: number = 10;
 
-  constructor(private fb: FormBuilder) {}
-
   ngAfterViewInit(): void {
     console.log('>>> add autocomplete false');
     let inputs = document.querySelectorAll('input');
 
     console.log('>> ', inputs);
 
-    inputs.forEach( input => {
-      input.setAttribute('autocomplete','none')
-    } )
+    inputs.forEach((input) => {
+      input.setAttribute('autocomplete', 'none');
+    });
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.form.value);
 
     const emptyObject = {
-      name: ''
+      name: '',
     };
 
-    console.log('is empty?? ', _.isEmpty(emptyObject))
+    console.log('is empty?? ', _.isEmpty(emptyObject));
 
     for (const i of _.range(this.beneficiaries_per_disease)) {
-      console.log('i >>', i)
+      console.log('i >>', i);
     }
   }
   //* Cuando se envia un FormGroup
@@ -56,7 +68,63 @@ export class AppComponent implements AfterViewInit {
   // }
 
   //* Cuando se envia un FormGroup
-  logForm(control: AbstractControl | null){
-    console.log('control >>',control);
+  logForm(control: AbstractControl | null) {
+    console.log('control >>', control);
+  }
+
+  constructor(private fb: FormBuilder, private cd: ChangeDetectorRef) {
+    this.formList$.subscribe((_) => {
+      this.collapseAnimation(true, 'content');
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.formList$.unsubscribe();
+  }
+
+  formList: any[] = [];
+  formList$ = new BehaviorSubject<any[]>([]);
+  memoryFormList: any[] = [];
+
+  bool: boolean = false;
+
+  pushMethod() {
+    this.formList$.next([...this.formList$.value, this.form.value]);
+  }
+
+  collapseAnimation(value: boolean, elementId: string) {
+    let contentHeight = 0;
+    const animationDelay = 0;
+    const element: HTMLElement | null = document.getElementById(elementId);
+
+    const children: HTMLCollection | never[] = document.getElementById(elementId)?.children || [];
+
+    setTimeout(() => {
+      Array.from(children).forEach((item: Element) => {
+        contentHeight += item.clientHeight;
+
+      });
+      contentHeight += 20;
+      if (value) {
+        element?.style.setProperty('max-height', `${contentHeight}px`);
+      } else {
+        element?.style.setProperty('max-height', '0');
+      }
+    }, animationDelay);
+  }
+
+  deleteItem(index: number, elementId: string) {
+    const cloneArray = _.cloneDeep(this.formList$.value);
+    this.deleteItemAnimation(false, elementId);
+    setTimeout(() => {
+      cloneArray.splice(index, 1);
+      this.formList$.next(cloneArray);
+    }, 500);
+  }
+
+  deleteItemAnimation(value: boolean, elementId: string) {
+    const element: HTMLElement | null = document.getElementById(elementId);
+
+    element?.style.setProperty('max-height', '0');
   }
 }
