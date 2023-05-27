8e93662e1e4552b4ba67173ad0f52f3806af577b
lelequinter <lele.quinter@gmail.com>
2023-05-27 11:53:28 -0500

    implement text to speech

--- src/app/pages/text-to-speech/text-to-speech.component.ts
+++ src/app/pages/text-to-speech/text-to-speech.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text-to-speech',
   templateUrl: './text-to-speech.component.html',
-  styleUrls: ['./text-to-speech.component.css']
+  styleUrls: ['./text-to-speech.component.css'],
 })
-export class TextToSpeechComponent {
+export class TextToSpeechComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const s = this.getVoices();
+    s.then((voices: any) => {
+      this.voices = voices;
+    }) ;
+  }
 
+  //* Inicializacion del speechSynthesis
+  synth = window.speechSynthesis;
+
+  text: string =
+    'Fired when the utterance has begun to be spoken. Also available via the onstart property.';
+
+  textSplitted: string[] = this.text.split(' ');
+
+  voices: any[] = [];
+
+  getVoices() {
+    return new Promise(function (resolve, reject) {
+      let synth = window.speechSynthesis;
+      let id: any;
+
+      id = setInterval(() => {
+        if (synth.getVoices().length !== 0) {
+          resolve(synth.getVoices());
+          clearInterval(id);
+        }
+      }, 10);
+    });
+  }
+
+  speechText(text: string) {
+    const utterThis = new SpeechSynthesisUtterance(text);
+
+    const voiceSelect: HTMLSelectElement  | null = document.querySelector("select");
+    let voiceindex: number = Number(voiceSelect?.value || 3);
+
+    utterThis.voice = this.voices[voiceindex];
+
+    this.synth.speak(utterThis);
+  }
 }
