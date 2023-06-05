361e6f39821102037b01a1685d97f1a17659f357
lelequinter <lele.quinter@gmail.com>
2023-06-04 19:23:09 -0500

    click inside element

--- src/app/pages/learn/learn.component.ts
+++ src/app/pages/learn/learn.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscriber, fromEvent, map, observable } from 'rxjs';
 
 @Component({
   selector: 'app-learn',
   templateUrl: './learn.component.html',
   styleUrls: ['./learn.component.css']
 })
-export class LearnComponent {
+export class LearnComponent implements OnInit {
+  click$ = fromEvent<PointerEvent>(document, 'click');
+
+  ngOnInit(): void {
+    this.click$
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          const modal = document.getElementById("target");
+          const sita = this.clickInside(res, modal);
+          console.log('@sita',sita);
+
+        }
+      })
+  }
+
+  clickInside(event: PointerEvent, notelem: any)	{
+    return event.target == notelem || notelem.contains(event.target);
+  }
 
 }
