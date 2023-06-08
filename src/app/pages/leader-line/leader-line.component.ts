172870d3aec2f163f9239e176d94006e4acc020f
Leyder Quintero <sistemas78@red5g.co>
2023-06-07 19:22:33 -0500

    importacion de leader-line library

--- src/app/pages/leader-line/leader-line.component.ts
+++ src/app/pages/leader-line/leader-line.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-leader-line',
   templateUrl: './leader-line.component.html',
   styleUrls: ['./leader-line.component.css']
 })
-export class LeaderLineComponent {
+export class LeaderLineComponent implements OnInit {
+  ngOnInit(): void {
+    const win = (window as any);
 
+    console.log('@win', win);
+  }
 }
